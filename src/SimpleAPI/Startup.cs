95effbae131d3b711e459673812832924275c4de
Vincent.Nguyen <Vincent.Nguyen@wealth-dynamix.com>
2023-01-17 13:33:20 +0700

    add UseHttpsRedirection

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -40,7 +40,9 @@ namespace SimpleAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 
                 app.UseHsts();
-            }            
+            }
+
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
