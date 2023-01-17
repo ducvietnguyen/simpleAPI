ebc322078405d2eb68546c6dae61fde2e2161ef3
Vincent.Nguyen <Vincent.Nguyen@wealth-dynamix.com>
2023-01-17 13:21:40 +0700

    commit

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -38,6 +38,7 @@ namespace SimpleAPI
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
 
