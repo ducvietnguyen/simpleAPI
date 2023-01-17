7a9ffef08e2052f8f02bd35352d7832b0e150c01
Vincent.Nguyen <Vincent.Nguyen@wealth-dynamix.com>
2023-01-17 14:11:19 +0700

    fix test

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace SimpleAPI.Test
         public void GetReturnsMyName()
         {
             var returnValue = controller.Get(1);
-            Assert.Equal("Les Jackson 2", returnValue.Value);
+            Assert.Equal("Les Jackson", returnValue.Value);
         }
 
 
